8208ac5cc0638532dfacce8a30524d3f871076dd
Pedro <pedro.penha@multimidiaeducacional.com.br>
2026-09-10 17:22:20 -0300

    chore(config): 🔧 update local home base URL IP address

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,7 +2,7 @@ enum Environment { local, localCasa, test, production, beta }
 
 class ApiConfig {
   static const String _localBaseUrl = 'http://192.168.10.175:8080';
-  static const String _localBaseUrlCasa = 'http://192.168.1.10:8080';
+  static const String _localBaseUrlCasa = 'http://192.168.1.12:8080';
   static const String _testBaseUrl =
       'https://test-api.multimidiaeducacional.com.br';
   static const String _productionBaseUrl =
